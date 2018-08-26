bd64b393a306afc9e64e4f22c269e8ff12d6194b
michael_townsend <michael.h.townsend@gmail.com>
2018-08-26 19:07:56 -0400

    Implemented removing duplicates

--- script.py/script.py/script.py.py
+++ script.py/script.py/script.py.py
@@ -27,7 +27,7 @@ def RunQueryGetJSON(url, apiKey):
     global requestsRemaining
     urlBase = url.format(apiKey)
 
-    conn = http.client.HTTPSConnection("api.themoviedb.org")
+    conn = http.client.HTTPSConnection("api.themoviedb.org", timeout=10000)
 
     payload = "{}"
 
@@ -37,10 +37,10 @@ def RunQueryGetJSON(url, apiKey):
     # check to see if we hit request limit and need to wait out current 10 second interval
     CheckRateLimit(response)
 
-    #print("Num requests remainig: " + str(requestsRemaining))
+    #print("Num requests remaining: " + str(requestsRemaining))
     if int(requestsRemaining) < 1:
         time.sleep(2)
-        RunQueryGetJSON(url, apiKey)
+        return RunQueryGetJSON(url, apiKey)
 
     data = response.read().decode("utf-8")
     return json.loads(data)
@@ -56,11 +56,10 @@ def GetComedyMovies(apiKey, comedyID):
         jsonObj = RunQueryGetJSON(url, apiKey)
         comedies.extend(FilterComedyMovies(jsonObj['results'], comedyID))
         page = page + 1
-        print(str(len(comedies)))
     return comedies[:300]
 
 def WriteToCSV(fileName, dataList):
-    with open(fileName, mode='w') as csv_file:
+    with open(fileName, mode='w', newline='') as csv_file:
         csvWriter = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
         for row in dataList:
             csvWriter.writerow(row)
@@ -70,9 +69,53 @@ def ReturnValues(item, keys):
     for key in keys:
         values.append(item[key])
     return values
-    #id = item['id']
-    #name = item['title']
-    #return [id, name]
+
+def FindSimilarMovies(movieList, apiKey):
+    movieDetailsList = []
+    for movie in movieList:
+        #print("Movies similar to: " + movie[1])
+        url = "/3/movie/" + str(movie[0]) + "/similar?page=1&language=en-US&api_key={0}"
+        jsonObj = RunQueryGetJSON(url, apiKey)
+        movieDetails = ExtractMovieDetails(jsonObj['results'][:5])
+        movieDetailsList.extend(list(map(lambda movieDetail: MapSimilarMovies(movieDetail, movie[0]),movieDetails)))
+    return movieDetailsList
+
+def MapSimilarMovies(movieDetail, movieID):
+    return [movieID, movieDetail[0]]
+
+def ExtractMovieDetails(moviesList):
+    keys = ['id','title']
+    return list(map(lambda row: ReturnValues(row, keys), moviesList))
+
+def RemoveDuplicates(movieList):
+    trimmedSet = []
+    seenMovies = []
+    for movie1 in range(0, len(movieList) -1):
+        movie1ID = movieList[movie1][0]
+        movie1SimilarID = movieList[movie1][1]
+        alreadyAdded = False
+        for movie2 in range(movie1+1, len(movieList)):
+            movie2ID = movieList[movie2][0]
+            movie2SimilarID = movieList[movie2][1]
+            movieIDsSame = movie1ID == movie2SimilarID
+            movieSimilarSame = movie2ID == movie2SimilarID
+            seenBefore = movie2ID in seenMovies
+            if movieIDsSame and movieSimilarSame and seenBefore:
+                alreadyAdded = True
+
+        if alreadyAdded == False:
+            trimmedSet.append(movieList[movie1])
+            seenMovies.append(movie1ID)
+
+    return trimmedSet
+
+def NotSeenYet(movieList, movieDetails):
+    haventSeen = True
+    for movie in movieList:
+        if (movie[0] == movieDetails[0]) and (movie[1] == movieDetails[1]):
+            haventSeen = False
+    return haventSeen
+        
 
 def main():
     args = sys.argv
@@ -82,10 +125,19 @@ def main():
     #print(jsonObj)
     comedyID = GetComedyID(jsonObj['genres'])
     comedies = GetComedyMovies(args[1], comedyID)
-    keys = ['id','title']
-    comedyValues = map(lambda row: ReturnValues(row, keys), comedies)
-    #print(comedyValues)
-    WriteToCSV("C:\Development\CSE6242\CSE6242_Hwk1\Files\movie_ID_name.csv",comedyValues)
+    comedyDetails = ExtractMovieDetails(comedies)
+    #print(len(list(comedyValues)))
+    WriteToCSV("movie_ID_name.csv", comedyDetails)
+
+    # Get similar movie
+    movieDetailsList = FindSimilarMovies(comedyDetails, args[1])
+    WriteToCSV("movie_ID_sim_movie_ID_with_dupes.csv", movieDetailsList)
+
+    trimmedList = RemoveDuplicates(movieDetailsList)
+    WriteToCSV("movie_ID_sim_movie_ID.csv", trimmedList)
+
+    #WriteToCSV("movie_ID_sim_movie_ID.csv", movieDetailsList)
+
     input("Press enter to continue")
     exit()
 
