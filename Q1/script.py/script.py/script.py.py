5748eaa49a5f88cee4db7af418abcd343997eb0f
michael_townsend <michael.h.townsend@gmail.com>
2018-09-03 22:50:07 -0400

    Added Q2, added file structure

--- Q1/script.py/script.py/script.py.py
+++ Q1/script.py/script.py/script.py.py
@@ -94,7 +94,7 @@ def RemoveDuplicates(movieList):
         movie1ID = movieList[movie1][0]
         movie1SimilarID = movieList[movie1][1]
         if AlreadySeen(trimmedSet, movie1ID, movie1SimilarID):
-                WriteToCSV("Duplicates.csv", [movieList[movie1]], 'a')
+                WriteToCSV("Duplicates.csv", [movieList[movie1\]], 'a')
         else:
             trimmedSet.append(movieList[movie1])
             #seenMovies.append(movie1ID)
